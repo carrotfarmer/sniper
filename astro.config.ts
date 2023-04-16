ae180f04c2f9f4e5227fd8b3374a34d06f2e95a1
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-04-16 15:54:11 +0530

    fix: formatting

--- astro.config.ts
+++ astro.config.ts
@@ -7,27 +7,30 @@ import prefetch from "@astrojs/prefetch";
 import "@fontsource/jetbrains-mono";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://www.astro-theme-cactus.netlify.app/",
-  markdown: {
-    shikiConfig: {
-      theme: "dracula",
-      wrap: true
-    }
-  },
-  integrations: [mdx({}), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), prefetch(), react()],
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+	site: "https://www.astro-theme-cactus.netlify.app/",
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
